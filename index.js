53a2baee446128340de17afcfa9ba357b5e7ffc1
Nalini Karina Andhini <nalinikarina613@gmail.com>
2026-06-19 05:53:30 +0700

    js

---
+++ index.js
@@ -0,0 +1,23 @@
+const sentences = [
+  'Mauris ultricies sed sapien eget malesuada. Aliquam a lorem urna. Donec.',
+];
+
+function result(sentences) {
+  let max = 0;
+
+  for (let i = 0; i < sentences.length; i++) {
+    let sentence = sentences[i];
+    // let word = sentence.trim().split(" ");
+    let word = sentence.trim().split(/\s+/).length;
+
+    // if (word.length > max) {
+    //   max = word.length;
+    // }
+    if (word > max) {
+        max = word
+    }
+  }
+
+  return max;
+}
+console.log(result(sentences))
